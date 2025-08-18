8b1ddc4d3bc965b49c4f4da90a3399930be8dbed
VIVEK NISHAD <vknitk2026@gmail.com>
2025-08-18 12:17:39 +0530

    feat(junction table):add user_roles for m:n relationship of user and roles tables

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const {PORT}=require ("./config/ServerConfig")
-const app=express()
 const ApiRoutes= require("./routes/index")
 const bodyParser= require("body-parser")
+const db = require("./models")
 const prepareStartServer =() =>{
+
+const app=express()
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}))
@@ -11,6 +13,9 @@ app.use("/api",ApiRoutes)
 app.listen(PORT, async ()=>{
     console.log(`app started on port ${PORT}`)
 
+    // if(process.env.DB_SYNC){
+    //     db.sequelize.sync({alter:true})
+    // }
 
 })
 
---
+++ src/seeders/20250817180414-add-roles.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+   
+       await queryInterface.bulkInsert('Roles', [
+  {
+    name: 'ADMIN',
+    createdAt: new Date(),
+    updatedAt: new Date()
+  },
+  {
+    name: 'AIRLINE_BUISNESS', // Note: "BUSINESS" is misspelled here
+    createdAt: new Date(),
+    updatedAt: new Date()
+  },
+  {
+    name: 'CUSTOMER',
+    createdAt: new Date(),
+    updatedAt: new Date()
+  }
+], {});
+  
+
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
